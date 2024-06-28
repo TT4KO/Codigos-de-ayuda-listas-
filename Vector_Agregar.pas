19de9a426d8862d83f4b51192fa52b7c9a2aaad7
TT4KO <MateoQS4@hotmail.com>
2024-06-27 21:58:04 -0300

    Vector_Agregar.pas

---
+++ Vector_Agregar.pas
@@ -0,0 +1,10 @@
+Procedure agregar (var a :números; var dL:integer; var pude:boolean; num:integer);
+Begin
+  pude:= false;
+  if ((dl + 1) <= física) then 
+   begin
+     pude:= true;
+     dL:= dL + 1;
+     a[dL]:= num;
+   end; 
+end.
